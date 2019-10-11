74fa331addb74d2246a345b1ea95b8e51e8db97f
Tyler Gilchrist <tyler668@gmail.com>
2019-10-11 13:33:02 -0400

    All crud working

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-// const ProjectRouter = require('./schemes/scheme-router.js');
+const ProjectRouter = require('./projects/projects-router.js');
 
 const server = express();
 
 server.use(express.json());
-// server.use('/api/projects', ProjectRouter);
+server.use('/api/projects', ProjectRouter);
 
 server.get('/', (req, res) =>{
     res.status(200).json({Message: "You've reached the server 4242"});
